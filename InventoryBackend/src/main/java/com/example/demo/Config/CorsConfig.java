e168c2c75abc62a2f74bc45b0cca512273f50bb2
linh <klinh2212112@gmail.com>
2024-05-27 14:33:19 +0700

    config cors

--- InventoryBackend/src/main/java/com/example/demo/Config/CorsConfig.java
+++ InventoryBackend/src/main/java/com/example/demo/Config/CorsConfig.java
@@ -7,13 +7,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
+//    @Bean
+//    public WebMvcConfigurer webMvcConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**"); //Enable CORS for all endpoints
+//            }
+//        };
+//    }
     @Bean
     public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**"); //Enable CORS for all endpoints
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Add allowed HTTP methods
+                        .allowCredentials(true);;
             }
         };
     }
--- InventoryBackend/src/main/java/com/example/demo/Controller/AuthController.java
+++ InventoryBackend/src/main/java/com/example/demo/Controller/AuthController.java
@@ -4,10 +4,7 @@ import com.example.demo.Dto.ReqRes;
 import com.example.demo.Service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
